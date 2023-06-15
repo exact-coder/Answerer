09ea10d490549286458762002157d4641db4a37a
Programming Bees <hasanakash799@gmail.com>
2023-06-15 18:26:50 +0600

    added all pages

--- landing/urls.py
+++ landing/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("",home,name="home")
+    path("",home,name="home"),
+    path("about/",about,name="about"),
+    path("blog/",blog,name="blog"),
+    path("contact/",contact,name="contact"),
+    path("service/",service,name="service"),
 ]
--- landing/views.py
+++ landing/views.py
@@ -3,4 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,"landing/index.html")
+    return render(request,"landing/pages/home.html")
+
+def about(request):
+    return render(request,"landing/pages/about.html")
+
+def blog(request):
+    return render(request,"landing/pages/blog.html")
+
+def contact(request):
+    return render(request,"landing/pages/contact.html")
+
+def service(request):
+    return render(request,"landing/pages/service.html")
+
