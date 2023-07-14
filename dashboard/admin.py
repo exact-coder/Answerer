16c05e5b2b82fdbed68c1087799c4c911de572a3
Programming Bees <hasanakash799@gmail.com>
2023-07-14 22:23:13 +0600

    chore: created profile models

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['uniqueId','user','position_title','call','slug','last_updated']
+    list_display_links = ['uniqueId','user','position_title']
--- dashboard/models.py
+++ dashboard/models.py
@@ -18,5 +18,24 @@ class Profile(models.Model):
     call = models.IntegerField(_("Call"),max_length=16)
     address = models.CharField(_("Address"), max_length=200)
     description = models.TextField(_("Descrive Yourself's"))
+    profileImage = ResizedImageField(size=[200,200],quality=80, upload_to="profile/")
+    uniqueId = models.UUIDField(primary_key = True,default =uuid4,editable = False)
+    slug = models.SlugField(_("Slug"),max_length=500,unique=True,blank=True,null=True)
+
+    date_created = models.DateTimeField(_("Created Date"), auto_now=False, auto_now_add=True)
+    last_updated = models.DateTimeField(_("Last Updated"), auto_now=True, auto_now_add=False)
+
+    def __str__(self) -> str:
+        return '{} {} {}'.format(self.user.first_name,self.user.last_name,self.user.email)
     
+    def save(self,*args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+        self.slug = slugify('{} {} {}'.format(self.user.first_name,self.user.last_name, self.user.email))
+        self.last_updated = timezone.localtime(timezone.now())
+        super(Profile, self).save(*args, **kwargs)
+
+
 
